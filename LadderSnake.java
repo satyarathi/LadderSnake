2bdd5157b50f75f01ec7226d865d630c8dc272fa
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-03 03:40:39 +0530

    second

--- LadderSnake.java
+++ LadderSnake.java
@@ -3,8 +3,23 @@ import java.util.Random;
 
 public class LadderSnake {
 
+	public int Diceroll()
+	{
+	    int num = 0;
+	    Random dr = new Random();
+	    num = dr.nextInt(7);
+	    if (num == 0) {
+	    	num++;
+	    }
+	    System.out.println("Dice value:" +num);
+	    return (num);
+	    
+	}
 	public static void main(String[] args) {
-		
 		System.out.println("Position starts at 0");
+		
+		//diceroll
+		LadderSnake n = new LadderSnake();
+		n.Diceroll();
 	}
 }
